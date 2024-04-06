aad55ce4a778bae9d339c1ee37186c694ce7f895
= <=>
2024-04-06 13:29:41 +0200

    Remove unnecessary labs for lab6 branch

--- Lab7/exercise-3/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('#tree-container h2 span, #tree-container h3 span').forEach(span => {
-    span.addEventListener('click', function() {
-      const childUl = this.parentNode.nextElementSibling;
-      if (childUl && childUl.tagName.toLowerCase() === 'ul') {
-        childUl.classList.toggle('visible');
-      }
-    });
-  });
-  
